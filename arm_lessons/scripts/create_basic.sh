e74be6441b2dc396761e23a45d887221a43976e7
M0KApointstoint <mocanuc579@gmail.com>
2026-08-31 18:56:34 +0300

    Fix small typos.

--- arm_lessons/scripts/create_basic.sh
+++ arm_lessons/scripts/create_basic.sh
@@ -7,5 +7,5 @@ mv simple.s $1
 echo -e "\e[0;33mProgram contents loading:\e[0m\n"
 sleep 1
 cat $1
-echo -e "\e[0;32mProgram created succesffully!\e[0m\n"
+echo -e "\e[0;32mProgram created successffully!\e[0m\n"
 
