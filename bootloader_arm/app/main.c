e7883ff4c5bb1a7b6200da28e14a200c94bee09f
M0KApointstoint <mocanuc579@gmail.com>
2026-09-15 00:37:43 +0300

    Create minimal app firmware.

---
+++ bootloader_arm/app/main.c
@@ -0,0 +1,36 @@
+#include <stdint.h>
+
+#define SRAM_BASE 0x20000000u
+#define SRAM_LENGTH (64u * 1024u)
+#define STACK_TOP (SRAM_BASE + SRAM_LENGTH)
+
+#define UART0_DR (*(volatile uint32_t *)0x4000c000u)
+
+void app_reset_handler(void);
+
+__attribute__((section(".vector_table"))) uint32_t vector_table[] = {
+	STACK_TOP,
+	(uint32_t)app_reset_handler
+};
+
+void uart_putc(char c)
+{
+	UART0_DR = (uint32_t)c;
+}
+
+void uart_puts(const char *buf)
+{
+	while (*buf) {
+		uart_putc(*buf);
+		++buf;
+	}
+}
+
+void app_reset_handler(void)
+{
+	uart_puts("\n APP STARTED!!! \n");
+
+	while (1) {
+	}
+}
+
